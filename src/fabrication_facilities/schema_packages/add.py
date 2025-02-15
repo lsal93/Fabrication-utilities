fd000d83bfb176bef7381dbaa71d10ada56e53c1
Trog-404 <bontornomatteo@gmail.com>
2025-02-15 21:21:44 +0000

    Spin coating iniziato

--- src/fabrication_facilities/schema_packages/add.py
+++ src/fabrication_facilities/schema_packages/add.py
@@ -168,7 +168,193 @@ class ICP_CVD(FabricationProcessStep, Chemical, ArchiveSection):
 
 
 class Spin_Coating(FabricationProcessStep, Chemical, ArchiveSection):
-    pass
+    m_def = Section(
+        a_eln={
+            'hide': [
+                'description',
+                'lab_id',
+                'datetime',
+                'comment',
+                'duration',
+            ],
+            'properties': {
+                'order': [
+                    'name',
+                    'job_progressive_id',
+                    'start_time',
+                    'ending_date',
+                    'fabricationProcessStepDefinition',
+                    'fabricationEquipmentRecipeName',
+                    'thickness_from_recipe',
+                    'duration_from_recipe',
+                    'short_name',
+                    'chemical_formula',
+                    'thickness_target',
+                    'hdms_required',
+                    'exposure_required',
+                    'exposure_duration',
+                    'peb_required',
+                    'peb_duration',
+                    'peb_temperature',
+                    'dewetting_duration',
+                    'dewetting_temperature',
+                    'spin_dispensed_volume',
+                    'spin_frequency',
+                    'spin_acceleration',
+                    'spin_duration',
+                    'thickness_obtained',
+                    'notes',
+                ]
+            },
+        },
+    )
+    thickness_from_recipe = Quantity(
+        type=np.float64,
+        description='Amount of material deposited under standard conditions described in the recipe',
+        a_eln={'component': 'NumberEditQuantity', 'defaultDisplayUnit': 'um'},
+        unit='um',
+    )
+    duration_from_recipe = Quantity(
+        type=np.float64,
+        description='Time prescribed by the recipe',
+        a_eln={'component': 'NumberEditQuantity', 'defaultDisplayUnit': 'minute'},
+        unit='minute',
+    )
+    short_name = Quantity(
+        type=str,
+        description='Material to be deposited',
+        a_eln={'component': 'StringEditQuantity', 'label': 'photoresist name'},
+    )
+    chemical_formula = Quantity(
+        type=str,
+        description='Inserted only if known',
+        a_eln={'component': 'StringEditQuantity'},
+    )
+    thickness_target = Quantity(
+        type=np.float64,
+        description='Amount of material to be deposited',
+        a_eln={'component': 'NumberEditQuantity', 'defaultDisplayUnit': 'um'},
+        unit='um',
+    )
+    hdms_required = Quantity(
+        type=bool,
+        description='The recipe use the hdms?',
+        a_eln={
+            'component': 'BoolEditQuantity',
+        },
+    )
+    exposure_required = Quantity(
+        type=bool,
+        description='The recipe use exposure?',
+        a_eln={
+            'component': 'BoolEditQuantity',
+        },
+    )
+    exposure_duration = Quantity(
+        type=None,
+        description='The duration of the exposure',
+        a_eln={
+            'component': None,
+            'defaultDisplayUnit': None,
+        },
+        unit=None,
+    )
+    peb_required = Quantity(
+        type=bool,
+        description='The recipe use exposure?',
+        a_eln={
+            'component': 'BoolEditQuantity',
+        },
+    )
+    peb_duration = Quantity(
+        type=None,
+        description='The duration of the exposure',
+        a_eln={
+            'component': None,
+            'defaultDisplayUnit': None,
+        },
+        unit=None,
+    )
+    peb_temperature = Quantity(
+        type=None,
+        description='The duration of the exposure',
+        a_eln={
+            'component': None,
+            'defaultDisplayUnit': None,
+        },
+        unit=None,
+    )
+    dewetting_duration = Quantity(
+        type=np.float64,
+        description='The duration of the dewetting',
+        a_eln={
+            'component': 'NumberEditQuantity',
+            'defaultDisplayUnit': 'minute',
+        },
+        unit='minute',
+    )
+    dewetting_temperature = Quantity(
+        type=np.float64,
+        description='The temperaure of the dewetting',
+        a_eln={
+            'component': 'NumberEditQuantity',
+            'defaultDisplayUnit': 'celsius',
+        },
+        unit='celsius',
+    )
+    spin_dispensed_volume = Quantity(
+        type=np.float64,
+        description='Solution dispensed',
+        a_eln={
+            'component': 'NumberEditQuantity',
+            'defaultDisplayUnit': 'milliliter',
+        },
+        unit='milliliter',
+    )
+    spin_frequency = Quantity(
+        type=np.float64,
+        description='Velocity of the spinner',
+        a_eln={
+            'component': 'NumberEditQuantity',
+            'defaultDispalyUnit': 'rpm',
+        },
+        unit='rpm',
+    )
+    spin_angular_acceleration = Quantity(
+        type=np.float64,
+        description='Acceleration of the spinner',
+        a_eln={
+            'component': 'NumberEditQuantity',
+            'defaultDispalyUnit': 'rpm/s',
+        },
+        unit='rpm/s',
+    )
+    spin_duration = Quantity(
+        type=np.float64,
+        description='Acceleration of the spinner',
+        a_eln={
+            'component': 'NumberEditQuantity',
+            'defaultDispalyUnit': 's',
+        },
+        unit='s',
+    )
+    thickness_obtained = Quantity(
+        type=np.float64,
+        description='Amount of material deposited under standard conditions described in the recipe',
+        a_eln={'component': 'NumberEditQuantity', 'defaultDisplayUnit': 'um'},
+        unit='um',
+    )
+
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        """
+        The normalizer for the `Step` class.
+
+        Args:
+            archive (EntryArchive): The archive containing the section that is being
+            normalized.
+            logger (BoundLogger): A structlog logger.
+        """
+        super().normalize(archive, logger)
 
 
 m_package.__init_metainfo__()
