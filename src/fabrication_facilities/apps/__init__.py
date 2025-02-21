cff30c78466a0e78c23e7cbab8984e3968e022cc
Trog-404 <bontornomatteo@gmail.com>
2025-02-21 14:57:55 +0000

    Instrument modification

--- src/fabrication_facilities/apps/__init__.py
+++ src/fabrication_facilities/apps/__init__.py
@@ -1,23 +1,51 @@
 from nomad.config.models.plugins import AppEntryPoint
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
+from nomad.config.models.ui import (
+    App,
+    Menu,
+    MenuItemHistogram,
+    MenuItemTerms,
+)
 
 app_entry_point = AppEntryPoint(
-    name='NewApp',
-    description='New app entry point configuration.',
+    name='Fabrication findability',
+    description='New app entry point configuration for fabrication facilities.',
     app=App(
-        label='NewApp',
+        label='Equipments',
         path='app',
-        category='simulation',
-        columns=Columns(
-            selected=['entry_id'],
-            options={
-                'entry_id': Column(),
-            },
-        ),
-        filter_menus=FilterMenus(
-            options={
-                'material': FilterMenu(label='Material'),
-            }
-        ),
+        category='Fabrication facilities',
+        description='App to search fabrication equipments.',
+        readme=' The findability reach the level of the single technique at disposal.',
+        #        columns=Columns(
+        #            selected=[''],
+        #            options={
+        #                'entry_id': Column(),
+        #            },
+        #        ),
+        #    ),
+        #        menu=Menu(
+        #            size='sm',
+        #            items=[
+        #                MenuItemTerms(search_quantity='authors.name', options=5),
+        #                # This is a submenu whose items become visible once selected. It
+        #                # contains three items: one full-width histogram and two terms items
+        #                # which are displayed side-by-side.
+        #                Menu(
+        #                    title='Submenu',
+        #                    size='md',
+        #                    items=[
+        #                        MenuItemHistogram(search_quantity='upload_create_time'),
+        #                        # These items target data from a custom schema
+        #                        MenuItemTerms(
+        #                            width=6,
+        #                            search_quantity='data.quantity1#nomad_example.schema_packages.mypackage.MySchema',
+        #                        ),
+        #                        MenuItemTerms(
+        #                            width=6,
+        #                            search_quantity='data.quantity2#nomad_example.schema_packages.mypackage.MySchema',
+        #                        ),
+        #                    ],
+        #                ),
+        #            ],
+        #        ),
     ),
 )
