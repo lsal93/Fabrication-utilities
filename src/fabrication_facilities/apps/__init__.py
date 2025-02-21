8bac29468f689f7d6577bd983f4adf48f412723b
Trog-404 <bontornomatteo@gmail.com>
2025-02-21 15:36:06 +0000

    App

--- src/fabrication_facilities/apps/__init__.py
+++ src/fabrication_facilities/apps/__init__.py
@@ -32,7 +32,10 @@ app_entry_point = AppEntryPoint(
             size='sm',
             title='Equipment',
             items=[
-                MenuItemTerms(search_quantity='data.institution', options=5),
+                MenuItemTerms(
+                    search_quantity='data.institution#fabrication_facilities.schema_packages.equipment.Equipment',
+                    options=5,
+                ),
                 # This is a submenu whose items become visible once selected. It
                 # contains three items: one full-width histogram and two terms items
                 # which are displayed side-by-side.
