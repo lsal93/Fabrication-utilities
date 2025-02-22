3217814bff37f2d7038f7aa85cf3f1488fc71283
Trog-404 <bontornomatteo@gmail.com>
2025-02-22 09:13:16 +0000

    Second app

--- src/fabrication_facilities/apps/__init__.py
+++ src/fabrication_facilities/apps/__init__.py
@@ -1,8 +1,15 @@
 from nomad.config.models.plugins import AppEntryPoint
 from fabrication_facilities.apps.equipmentapp import equipmentapp
+from fabrication_facilities.apps.removeapp import processapp
 
 equipment_app_entry_point = AppEntryPoint(
     name='Fabrication_equipment_search',
     description='New app for equipment of fabrication facilities.',
     app=equipmentapp,
+)
+
+process_app_entry_point = AppEntryPoint(
+    name='Fabrication_process_search',
+    description='New app for equipment of fabrication facilities.',
+    app=processapp,
 )
---
+++ src/fabrication_facilities/apps/removeapp.py
@@ -0,0 +1,61 @@
+from nomad.config.models.ui import (
+    App,
+    Column,
+    Menu,
+    MenuItemCustomQuantities,
+    MenuItemTerms,
+    MenuItemHistogram,
+    Axis,
+    SearchQuantities,
+)
+
+dir = 'fabrication_facilities.schema_packages.fabrication_steps.FabricationProcess'
+Mainstr = 'data.equipmentTechniques.techniqueMainCategory'
+Substr = 'data.equipmentTechniques.techniqueSubCategory'
+cap = 'data.capabilities'
+itp = 'data.permittedItems'
+
+processapp = App(
+    label='Processes',
+    path='processesapp',
+    category='Fabrication facilities',
+    description='App to search fabrication processes.',
+    readme=' The findability reach the level of the step.',
+    search_quantities=SearchQuantities(
+        include=[f'*#{dir}'],
+    ),
+    columns=[
+        Column(quantity='entry_name', selected=True),
+        Column(quantity='entry_type', selected=True),
+        Column(
+            quantity=f'data.institution#{dir}',
+            selected=True,
+        ),
+        Column(quantity='upload_create_time', selected=True),
+    ],
+    filters_locked={'section_defs.definition_qualified_name': dir},
+    menu=Menu(
+        items=[
+            Menu(
+                title='Infrastrucure',
+                indentation=0,
+                items=[
+                    MenuItemTerms(
+                        title='Institution',
+                        type='terms',
+                        search_quantity=f'data.institution#{dir}',
+                    ),
+                ],
+            ),
+            Menu(
+                title='User defined quantities',
+                items=[
+                    MenuItemCustomQuantities(
+                        title='Costumer user quantities',
+                        type='custom_quantities',
+                    ),
+                ],
+            ),
+        ],
+    ),
+)
