53c7f7b0fe217f0602642b5db81075dc6bfbffa2
Trog-404 <bontornomatteo@gmail.com>
2025-02-24 13:39:51 +0000

    Process

--- src/fabrication_facilities/apps/processapp.py
+++ src/fabrication_facilities/apps/processapp.py
@@ -8,19 +8,33 @@ from nomad.config.models.ui import (
 )
 
 dir = 'fabrication_facilities.schema_packages.fabrication_steps.FabricationProcess'
-dir1 = 'fabrication_facilities.schema_packages.add.ICP_CVD'
 processapp = App(
     label='Processes',
     path='processesapp',
     category='Fabrication facilities',
     description='App to search fabrication processes.',
-    readme=' The findability reach the level of the step.',
-    search_quantities=SearchQuantities(include=[f'*#{dir}', f'*#{dir1}']),
+    readme="""
+    This research app allows to search general information about fabrication processes.
+    You can search products, affiliation of the project and the item processed
+    """,
+    search_quantities=SearchQuantities(include=[f'*#{dir}']),
     columns=[
         Column(quantity='entry_name', selected=True),
-        Column(quantity='entry_type', selected=True),
+        Column(quantity='entry_type'),
         Column(
             quantity=f'data.affiliation#{dir}',
+            selected=True,
+        ),
+        Column(
+            quantity=f'data.id_proposal#{dir}',
+            selected=True,
+        ),
+        Column(
+            quantity=f'data.generic_product_name#{dir}',
+            selected=True,
+        ),
+        Column(
+            quantity=f'data.id_processed_item#{dir}',
             selected=True,
         ),
         Column(quantity='upload_create_time', selected=True),
@@ -28,46 +42,25 @@ processapp = App(
     filters_locked={'section_defs.definition_qualified_name': dir},
     menu=Menu(
         items=[
-            Menu(
-                title='Infrastrucure',
-                indentation=0,
-                items=[
-                    MenuItemTerms(
-                        title='Affiliation',
-                        type='terms',
-                        search_quantity=f'data.affiliation#{dir}',
-                    ),
-                    MenuItemTerms(
-                        title='ID proposal',
-                        type='terms',
-                        search_quantity=f'data.id_proposal#{dir}',
-                    ),
-                    MenuItemTerms(
-                        title='Product Type',
-                        type='terms',
-                        search_quantity=f'data.generic_product_name#{dir}',
-                    ),
-                    MenuItemTerms(
-                        title='ID item processed',
-                        type='terms',
-                        search_quantity=f'data.id_item_processed#{dir}',
-                    ),
-                ],
+            MenuItemTerms(
+                title='Affiliation',
+                type='terms',
+                search_quantity=f'data.affiliation#{dir}',
             ),
-            Menu(
-                title='synthesis',
-                items=[
-                    Menu(
-                        title='Filters for ICP_CVD processes',
-                        items=[
-                            MenuItemTerms(
-                                title='formula',
-                                type='terms',
-                                search_quantity=f"data.steps[?m_def=='{dir1}'].chemical_formula#{dir}",
-                            )
-                        ],
-                    )
-                ],
+            MenuItemTerms(
+                title='ID proposal',
+                type='terms',
+                search_quantity=f'data.id_proposal#{dir}',
+            ),
+            MenuItemTerms(
+                title='Product Type',
+                type='terms',
+                search_quantity=f'data.generic_product_name#{dir}',
+            ),
+            MenuItemTerms(
+                title='ID item processed',
+                type='terms',
+                search_quantity=f'data.id_item_processed#{dir}',
             ),
             Menu(
                 title='User defined quantities',
