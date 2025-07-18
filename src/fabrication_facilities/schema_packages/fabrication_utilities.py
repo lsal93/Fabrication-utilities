d14c7b23c86785cd35b285bb7424830714e52ef1
Trog-404 <bontornomatteo@gmail.com>
2025-07-18 19:57:55 +0200

    removed unfinished class

--- src/fabrication_facilities/schema_packages/fabrication_utilities.py
+++ src/fabrication_facilities/schema_packages/fabrication_utilities.py
@@ -601,8 +601,6 @@ class EquipmentReference(Link, ArchiveSection):
         a_eln={'component': 'ReferenceEditQuantity'},
     )
 
-class ItemPlacement(PlotSection)
-
 
 class FabricationProcessStep(FabricationProcessStepBase, EntryData):
     m_def = Section(
