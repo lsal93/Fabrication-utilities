c4e96e57d9dc971e6045a8711568a18bffa200bf
Trog-404 <bontornomatteo@gmail.com>
2025-08-08 15:31:00 +0200

    Nuova prova

--- src/fabrication_facilities/apps/__init__.py
+++ src/fabrication_facilities/apps/__init__.py
@@ -3,6 +3,7 @@ from nomad.config.models.plugins import AppEntryPoint
 from fabrication_facilities.apps.equipmentapp import equipmentapp
 from fabrication_facilities.apps.processapp import processapp
 from fabrication_facilities.apps.stepapp import stepapp
+from fabrication_facilities.apps.removeapp import removeapp
 
 equipment_app_entry_point = AppEntryPoint(
     name='Fabrication_equipment_search',
@@ -22,10 +23,10 @@ step_app_entry_point = AppEntryPoint(
     app=stepapp,
 )
 
-#app_remove_entry_point = AppEntryPoint(
-#    name='Remove steps research app',
-#    description='App for searching remove steps like etching, drying, etc.',
-#    app=removeapp,
-#)
+app_remove_entry_point = AppEntryPoint(
+    name='Remove steps research app',
+    description='App for searching remove steps like etching, drying, etc.',
+    app=removeapp,
+)
 
 app_entry_point = step_app_entry_point
