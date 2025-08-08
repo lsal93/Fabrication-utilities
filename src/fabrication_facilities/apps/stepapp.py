5dda66080d314b7411575368e7443e5a51224617
Trog-404 <bontornomatteo@gmail.com>
2025-08-08 15:19:52 +0200

    Removed removeapp for unexpected error

--- src/fabrication_facilities/apps/stepapp.py
+++ src/fabrication_facilities/apps/stepapp.py
@@ -40,34 +40,10 @@ from fabrication_facilities.apps.menu_steps import (
     menuutils_startingmaterial,
 )
 
-schemasrem = [f'*#{path_value}' for path_value in dir_path.values()]
-schemasrem = [
-    (
-        f'*#{path_value}'
-        for path_value in dir_path.values()
-        if '.steps.remove.' in path_value
-    )
-]
-
+schemas = [f'*#{path_value}' for path_value in dir_path.values()]
 fps = 'FabricationProcessStep'
 dir0 = f'fabrication_facilities.schema_packages.fabrication_utilities.{fps}'
 schemas.append(f'*#{dir0}')
-schemasrem.append(f'*#{dir0}')
-
-columns_to_use=[
-    Column(quantity='entry_name', selected=True),
-    Column(quantity='entry_type', selected=True),
-    Column(
-        quantity=f'data.affiliation#{dir0}',
-        selected=True,
-    ),
-    Column(
-        quantity=f'data.location#{dir0}',
-        selected=True,
-    ),
-    Column(quantity='upload_create_time', selected=True),
-    Column(quantity=f'data.recipe_name#{dir0}'),
-],
 
 stepapp = App(
     label='Fabrication steps',
@@ -81,20 +57,20 @@ stepapp = App(
     technique. Navigation that consists in multiple technique is not allowed.
     """,
     search_quantities=SearchQuantities(include=schemas),
-    columns=columns_to_use#[
-    #     Column(quantity='entry_name', selected=True),
-    #     Column(quantity='entry_type', selected=True),
-    #     Column(
-    #         quantity=f'data.affiliation#{dir0}',
-    #         selected=True,
-    #     ),
-    #     Column(
-    #         quantity=f'data.location#{dir0}',
-    #         selected=True,
-    #     ),
-    #     Column(quantity='upload_create_time', selected=True),
-    #     Column(quantity=f'data.recipe_name#{dir0}'),
-    # ],
+    columns=[
+         Column(quantity='entry_name', selected=True),
+         Column(quantity='entry_type', selected=True),
+         Column(
+             quantity=f'data.affiliation#{dir0}',
+             selected=True,
+         ),
+         Column(
+             quantity=f'data.location#{dir0}',
+             selected=True,
+         ),
+         Column(quantity='upload_create_time', selected=True),
+         Column(quantity=f'data.recipe_name#{dir0}'),
+     ],
     filters_locked={'section_defs.definition_qualified_name': dir0},
     menu=Menu(
         items=[
@@ -168,29 +144,29 @@ stepapp = App(
                     ),
                 ],
             ),
-            # Menu(
-            #     title='Remove steps',
-            #     indentation=0,
-            #     items=[
-            #         Menu(
-            #             title='Etching',
-            #             items=[
-            #                 menuremove_rie,
-            #                 menuremove_icprie,
-            #                 menuremove_driebosch,
-            #                 menuremove_wetetching,
-            #                 menuremove_wetclean,
-            #                 menuremove_stripping,
-            #                 menuremove_resistdev,
-            #                 menuremove_spinresist,
-            #             ],
-            #         ),
-            #         Menu(
-            #             title='Drying',
-            #             items=[menuremove_rinsingdrying],
-            #         ),
-            #     ],
-            # ),
+            Menu(
+                title='Remove steps',
+                indentation=0,
+                items=[
+                    Menu(
+                        title='Etching',
+                        items=[
+                            menuremove_rie,
+                            menuremove_icprie,
+                            menuremove_driebosch,
+                            menuremove_wetetching,
+                            menuremove_wetclean,
+                            menuremove_stripping,
+                            menuremove_resistdev,
+                            menuremove_spinresist,
+                        ],
+                    ),
+                    Menu(
+                        title='Drying',
+                        items=[menuremove_rinsingdrying],
+                    ),
+                ],
+            ),
             Menu(
                 title='Characterization steps',
                 indentation=0,
@@ -218,55 +194,6 @@ stepapp = App(
                     ),
                 ],
             ),
-        ],
-    ),
-)
-
-
-removeapp = App(
-    label='Fabrication remove steps',
-    path='removeapp',
-    category='Fabrication facilities',
-    description='App to search remove fabrication steps.',
-    readme="""
-    This app is intended to navigate around the ecosystem of clean room fabrication
-    possible remove steps. At the beginning you can see all the fabrication steps
-    available in nomad and than through the filters you can specialize the research per
-    single remove technique. Navigation across different techniques is not allowed.
-    """,
-    search_quantities=SearchQuantities(include=schemasrem),
-    columns=columns_to_use
-    filters_locked={'section_defs.definition_qualified_name': dir0},
-    menu=Menu(
-        title='Remove steps',
-        indentation=0,
-        items=[
-            Menu(
-                title='Etching',
-                items=[
-                    menuremove_rie,
-                    menuremove_icprie,
-                    menuremove_driebosch,
-                    menuremove_wetetching,
-                    menuremove_wetclean,
-                    menuremove_stripping,
-                    menuremove_resistdev,
-                    menuremove_spinresist,
-                    ],
-                ),
-            Menu(
-                title='Drying',
-                items=[menuremove_rinsingdrying],
-            ),
-        ],
-    ),
-    Menu(
-        title='User defined quantities',
-        items=[
-            MenuItemCustomQuantities(
-                title='Costumer user quantities',
-                type='custom_quantities',
-            ),
-        ],
-    ),
+        ]
+    )
 )
